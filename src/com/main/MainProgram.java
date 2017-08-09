845b3b8ae0c77569ed035dc539d374c2df810989
laxmikanthreddyk <laxmikanthreddyk@gmail.com>
2017-08-09 00:44:23 -0400

    Solved Problem B

--- src/com/main/MainProgram.java
+++ src/com/main/MainProgram.java
@@ -9,13 +9,20 @@ public class MainProgram {
 
 	public static void main(String[] args) {
          
-		ProblemA();
-		
+		//ProblemA();
+		ProblemB();
 	}
 	public static void ProblemA()
 	{
 		CountingSheep c = new CountingSheep();
 		c.CountSheepProgram("C:\\GoogleProgramsFiles\\Input\\A-large-practice.in","C:\\GoogleProgramsFiles\\Output\\A-large-practice.out");
+	}
+	
+	public static void ProblemB()
+	{
+		PancakeRevenge p = new PancakeRevenge();
+		//System.out.println(p.PancakeRevenge("+--++-+--"));
+		p.PancakeRevengeProgram("/Users/laxmikanth/development/googleprograms/input/B-large-practice.in","/Users/laxmikanth/development/googleprograms/output/B-large-practice.out");
 	}
 	
 
